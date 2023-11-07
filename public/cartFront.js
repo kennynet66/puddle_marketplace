cc8a9c57945945980e1791219a3eb21d57699d3d
Kennynet66 <kennynet66@gmail.com>
2023-11-07 15:31:35 +0300

    Optimized cart

--- public/cartFront.js
+++ public/cartFront.js
@@ -1,49 +1,23 @@
-const addCartButtons = document.querySelectorAll('.cart');
-const cartBadge = document.querySelector('.cart-badge');
-const priceElements = document.querySelectorAll('#price');
-const priceDiv = document.querySelector('.total-cost')
-const quantityInput = document.querySelectorAll('.quantiy');
-const quantity = quantityInput.value
 
-console.log(quantity)
+const totalCostDiv = document.getElementById('total-cost');
+const cartBadge = document.getElementById('cart-badge');
+const cartDetails = document.getElementById('cart-details');
 
-let addCart = 0;
-
-addCartButtons.forEach(button => {
-  let isAddedToCart = false;
-
-  button.addEventListener('click', () => {
-    if (isAddedToCart) {
-      console.log(addCart);
-      button.style.backgroundColor = "";
-      button.innerHTML = "Add to cart";
-      cartBadge.innerHTML = addCart;
-    } else {
-      addCart += 1;
-      button.style.backgroundColor = "red";
-      button.innerHTML = "Remove from cart";
-      cartBadge.innerHTML = addCart;
-    }
-
-    isAddedToCart = !isAddedToCart; // Toggle the state
-  });
-});
-
-
+let cartItems = 0;
 let totalCost = 0;
+let currentCost = 0;
+function test(itemCost) {
+  cartItems += 1
+  currentCost += itemCost;
+  totalCost = currentCost;
 
-addCartButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const itemPrice = parseFloat(button.getAttribute('data-price'));
-    if (!isNaN(itemPrice)) {
-      if (button.classList.contains('added-to-cart')) {
-        totalCost -= itemPrice;
-        priceDiv.innerHTML= "ksh "+ totalCost
-      } else {
-        totalCost += itemPrice;
-        priceDiv.innerHTML= "ksh "+ totalCost
-      }
-    }
-    button.classList.toggle('added-to-cart');
-  });
-});
+  totalCostDiv.textContent = "TOTAL: " + totalCost;
+  cartBadge.innerHTML=cartItems; 
+}
+function test2(itemname, itemCost) {
+    const cartDetails = document.getElementById('cart-details');
+    const itemParagraph = document.createElement('p');
+    itemParagraph.textContent =itemname + " @ " + itemCost;
+    cartDetails.appendChild(itemParagraph);
+}
+totalCostDiv.textContent = "TOTAL: " + totalCost;
