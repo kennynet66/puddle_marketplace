58610fecf151acd9ef2f7e35519b861a9f2ef5c1
Kennynet66 <kennynet66@gmail.com>
2023-11-17 14:19:55 +0300

    Added files

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -1,11 +1,11 @@
 const Router = require('express');
 const itemController = require('../controllers/itemController');
 const { checkItem } = require('../middleware/itemCheck');
-const { addItem } = require('../middleware/authMiddleware');
+const { addItem, requireAuth } = require('../middleware/authMiddleware');
 
 const router = Router();
 
 router.get('/view', checkItem, itemController.get_view);
-router.get('/item/search', itemController.item_get);
+router.get('/item/search', requireAuth, itemController.item_get);
 
 module.exports = router
