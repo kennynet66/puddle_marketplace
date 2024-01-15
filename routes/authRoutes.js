c48572961b72606353ab38ed1b035b331840770d
Kenedy <kennynet66@gmail.com>
2024-01-15 16:29:03 +0300

    clean css

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,7 +5,7 @@ const { requireAuth } = require('../middleware/authMiddleware');
 const router = Router();
 
 router.get('/login', (req,res) => { res.render('login')});
-router.get('/signup', (req,res) => { res.render('login')});
+router.get('/signup', (req,res) => { res.render('signup')});
 router.post('/login', authController.login_post);
 router.post('/signup', authController.signup_post);
 router.get('/logout', authController.logout);
