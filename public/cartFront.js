726c4090f8ca5aa66fa44ee5f04d436ed0b14682
Kenedy <kennynet66@gmail.com>
2024-02-01 16:44:06 +0300

    super cart

--- public/cartFront.js
+++ public/cartFront.js
@@ -1,22 +1,107 @@
-const totalCostDiv = document.getElementById('total-cost');
-const cartBadge = document.getElementById('cart-badge');
-const cartDetails = document.getElementById('cart-details');
 
-let cartItems = 0;
+const displayCost = document.querySelector('.totalcost');
+const items = document.querySelectorAll('.addcartbtn');
+let update = document.querySelector('.update');
+const displayItem = document.querySelector('.displayitem');
+update.textContent = 0
+
+let cart = [];
+
+window.onload = () => {
+  let data = localStorage.getItem('cart');
+  data = JSON.parse(data)
+if (data) {
+  data.forEach((el) => {
+    cart.push(el)
+  })
+  displayCart()
+  doMath();
+} else {
+  doMath();
+  displayCart();
+}
+};
+
 let totalCost = 0;
 let currentCost = 0;
-function get_price(itemCost) {
-  cartItems += 1
-  currentCost += itemCost;
-  totalCost = currentCost;
 
-  totalCostDiv.textContent = "TOTAL: " + totalCost;
-  cartBadge.innerHTML = cartItems;
+update.textContent = '0';
+displayCost.textContent = 'Total Cost: 00';
+
+let badge = 0;
+
+items.forEach((item, index) => {
+  // console.log(item)
+  let price = parseFloat(item.getAttribute('price'));
+  let img = item.getAttribute('src');
+  item.addEventListener('click', () => {
+    let newItem = {itemName: item.name, itemPrice: price, image: img}
+    cart.push(newItem);
+    saveCart(cart);
+    displayCart()
+    doMath();
+  });
+});
+
+function saveCart(items) {
+  localStorage.setItem('cart', JSON.stringify(items));
 }
-function get_name(itemname, itemCost) {
-  const cartDetails = document.getElementById('cart-details');
-  const itemParagraph = document.createElement('p');
-  itemParagraph.textContent = itemname + " @ " + itemCost;
-  cartDetails.appendChild(itemParagraph);
+
+
+function displayCart(){
+  if(cart.length >= 1) {
+    displayItem.textContent = "";
+    cart.forEach((item) => {
+    update.textContent = badge;
+    let cartItem = document.createElement('div');
+      cartItem.className = "cartitem";
+
+      let prodImg = document.createElement('img');
+      prodImg.className = "prodimg";
+      prodImg.src = item.image;
+
+      let prodName = document.createElement('p');
+      prodName.className = "prodname";
+      prodName.textContent = item.itemName;
+
+      let prodPrice = document.createElement('p');
+      prodPrice.className = "prodprice";
+      prodPrice.textContent = `Ksh ${item.itemPrice}`;
+
+      let delBtn = document.createElement('button');
+      delBtn.className = 'del-btn';
+      delBtn.textContent = "Remove"
+      delBtn.addEventListener('click', () => {
+        delCartItem();
+      })
+
+      cartItem.appendChild(prodImg);
+      cartItem.appendChild(prodName);
+      cartItem.appendChild(prodPrice);
+      cartItem.appendChild(delBtn);
+
+      displayItem.appendChild(cartItem);
+    })
+  } else {
+    displayItem.textContent = "No items in your cart"
+  }
 }
-totalCostDiv.textContent = "TOTAL: " + totalCost;
+
+
+function doMath() {
+  let badge = 0;
+  let totalCost = 0
+  cart.forEach((el) => {
+    badge += 1
+    totalCost += el.itemPrice;
+  });
+  update.textContent = badge;
+  displayCost.textContent = `Total Cost: ${totalCost}`;
+}
+
+function delCartItem(index) {
+  cart.splice(index,1)
+  saveCart(cart)
+  doMath();
+  displayCart();
+}
